53da5afdce174eeba0ba2d999d5e1c8e4222be48
emrgncr <egencer20@ku.edu.tr>
2024-09-14 17:04:07 +0300

    add to c file

--- src/uct/base/uct_iface.c
+++ src/uct/base/uct_iface.c
@@ -998,3 +998,7 @@ uct_base_ep_connect_to_ep(uct_ep_h tl_ep,
 
     return uct_ep_connect_to_ep_v2(tl_ep, device_addr, ep_addr, &param);
 }
+
+void snoop_uct_send_proxy(void *ep, size_t size, const char *func_name){
+    return;
+}
